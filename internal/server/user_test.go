e4b1399bf094333f92573c6c5d874acf1cd42a8b
Malik Rawashdeh <malikrawashdeh5@gmail.com>
2025-02-24 09:39:39 -0600

    MR: added config and store mock

---
+++ internal/server/user_test.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"backend_real_estate/internal/database"
+	"backend_real_estate/util"
+	"time"
+)
+
+
+
+
+func randomUser() database.Users {
+
+	password := util.RandomString(6)
+	hashedPassword, _ := util.HashPassword(password)
+	
+	return database.Users{
+		ID:       int64(util.RandomInt(1, 1000)),
+		Username: util.RandomUsername(),
+		Email:    util.RandomEmail(),
+		Dob:	  util.RandomDOB(),
+		Role:     database.UserRole(util.RandomRole()),
+		HashedPassword: hashedPassword,
+		FirstName: util.RandomString(6),
+		LastName: util.RandomString(6),
+		CreatedAt: time.Now(),
+	}
+}
