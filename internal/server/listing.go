b5ffc5aab6020c5b07484a4640e5f645994249c3
mahikaperi <mahikaperi@tamu.edu>
2025-04-06 20:07:45 -0500

    Displaying listing endpoints creation

--- internal/server/listing.go
+++ internal/server/listing.go
@@ -9,8 +9,7 @@ import (
 )
 
 type listingDisplayResponse struct {
-	Owner     int64  `json:"Owner" binding:required"`
-	Address   string `json: Address" binding:required`
+	Address   string `json:"Address" binding:required`
 	City      string `json:"City" binding:required`
 	State     string `json:"State" binding:required`
 	Zipcode   int32  `json:"Zipcode" binding:required`
@@ -20,6 +19,36 @@ type listingDisplayResponse struct {
 
 func getListingDisplayResponse(display database.Properties) listingResponse {
 	return listingDisplayResponse{
-		Owner: 
+		Address:   display.Address,
+		City: 	   display.City,
+		State: 	   display.State,
+		Zipcode:   display.Zipcode,
+		Bedrooms:  display.Bedrooms,
+		Bathrooms: display.Bathrooms
 	}
+}
+
+// getListingDisplayHandler handles the reponse for get property information
+//
+// @Summary display properties
+// @Description get listing by property id
+// @Tags listing
+// @Accept json
+// @Produce json
+// @Param listingDisplayResponse body getListingByIDRequest true "get listig by property id"
+// @Success 200 {object} listingDisplayResponse
+// @Failure 400 {object} string
+// @Failure 500 {object} string
+// @Router /listing/getListings [get]
+func (s *Server) getListingDisplayHandler(c *gin.Context) {
+
+	listing, err := s.dbService.GetListingByID(c, req.PropertyID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	resp := getListingDisplayResponse(listing)
+
+	c.JSON(http.StatusOK, resp)
 }
